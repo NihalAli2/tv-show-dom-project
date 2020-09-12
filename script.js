c576070bc9be49ae461f23f2e7e247e968ef30f1
Nihalali <nihaluthman@hotmail.com>
2020-09-12 23:26:06 +0100

    First step

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  for (let i = 0; i < episodeList.length; i++) {
+    let episodDivEl = document.createElement("div");
+    let episodHeadEl = document.createElement("h3");
+    let imageEl = document.createElement("img");
+    let sumEpisoedEl = document.createElement("p");
+    episodHeadEl.textContent = episodeList[i].name + episodeList[i].season + episodeList[i].number;
+    episodDivEl.appendChild(episodHeadEl);
+    imageEl.src = episodeList[i].image.medium;
+    episodDivEl.appendChild(imageEl);
+    sumEpisoedEl.innerHTML = episodeList[i].summary;
+    episodDivEl.appendChild(sumEpisoedEl)
+    let origSorce = document.createElement("a");
+    origSorce.href = "https://tvmaze.com/";
+    origSorce.textContent = "This data is copy rigths, click here for more informathion";
+    episodDivEl.appendChild(origSorce)
 
+    rootElem.appendChild(episodDivEl);
+  }
+}
+let serchforEpi = document.getElementById("input");
+serchforEpi.addEventListener("keyup", function(event) {
+  input(event.target.value.toLowerCase())
+})
+function input(searchForEpis) {
+  let arrayForAllEpsi = allEpisodes.filter(episode => {
+    return episode.name.toLowerCase().includes(searchForEpis) || episode.summary.toLowerCase().includes(searchForEpis)
+  })
+makePageForEpisodes(arrayForAllEpsi);
+}
 window.onload = setup;
