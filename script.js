68e54e9b9f2e8dba228f241240c84f7967c98e0d
Nihalali <nihaluthman@hotmail.com>
2020-09-19 23:04:46 +0100

    New chang

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-//You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
+const allEpisodes = getAllEpisodes();
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   for (let i = 0; i < episodeList.length; i++) {
+    //Main div
     let episodDivEl = document.createElement("div");
+    //creat all episode head+image+summury
     let episodHeadEl = document.createElement("h3");
+
+    //image
     let imageEl = document.createElement("img");
+    //summury
     let sumEpisoedEl = document.createElement("p");
+
     episodHeadEl.textContent = episodeList[i].name + episodeList[i].season + episodeList[i].number;
     episodDivEl.appendChild(episodHeadEl);
     imageEl.src = episodeList[i].image.medium;
@@ -34,6 +40,27 @@ function input(searchForEpis) {
   let arrayForAllEpsi = allEpisodes.filter(episode => {
     return episode.name.toLowerCase().includes(searchForEpis) || episode.summary.toLowerCase().includes(searchForEpis)
   })
+
+
 makePageForEpisodes(arrayForAllEpsi);
+}
+
+//Drop down search box
+
+console.log(allEpisodes);
+function getselectEpisode(episodeSilector) {
+  
+  
+  for (let i = 0; i < allEpisodes.length; i++) {
+    let selectEpisode = document.getElementById("list");
+    let option = document.createElement("option");
+    option.value = i
+    option.textContent = allEpisodes[i].name;
+    selectEpisode.appendChild(option)
+   
+  }
+  
+
+getselectEpisode(selecEpis)
 }
 window.onload = setup;
