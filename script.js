94f6d1ee7d3f7cf307cee6a48b521823ad7d0164
NihalAli2 <62475392+NihalAli2@users.noreply.github.com>
2020-10-18 19:31:36 +0100

    add new changes

--- script.js
+++ script.js
@@ -1,66 +1,86 @@
+let allEpisodes;
+let allShowes;
 
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  allShowes = getAllShows();
+  fetch("https://api.tvmaze.com/shows/82/episodes")
+  
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    allEpisodes = data;
+  // const allEpisodes = getAllEpisodes();
+  console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
-}
-const allEpisodes = getAllEpisodes();
-function makePageForEpisodes(episodeList) {
+  getSelectEpisode();
+  })
+  getSelectShowes();
+  }
+
+  // const allEpisodes = getAllEpisodes();
+  
+  function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   for (let i = 0; i < episodeList.length; i++) {
-    //Main div
-    let episodDivEl = document.createElement("div");
-    //creat all episode head+image+summury
-    let episodHeadEl = document.createElement("h3");
-
-    //image
-    let imageEl = document.createElement("img");
-    //summury
-    let sumEpisoedEl = document.createElement("p");
-
-    episodHeadEl.textContent = episodeList[i].name + episodeList[i].season + episodeList[i].number;
-    episodDivEl.appendChild(episodHeadEl);
-    imageEl.src = episodeList[i].image.medium;
-    episodDivEl.appendChild(imageEl);
-    sumEpisoedEl.innerHTML = episodeList[i].summary;
-    episodDivEl.appendChild(sumEpisoedEl)
-    let origSorce = document.createElement("a");
-    origSorce.href = "https://tvmaze.com/";
-    origSorce.textContent = "This data is copy rigths, click here for more informathion";
-    episodDivEl.appendChild(origSorce)
-
-    rootElem.appendChild(episodDivEl);
+  //Main div
+  let episodDivEl = document.createElement("div");
+  //creat all episode head+image+summury
+  let episodHeadEl = document.createElement("h3");
+  
+  //image
+  let imageEl = document.createElement("img");
+  //summury
+  let sumEpisoedEl = document.createElement("p");
+  
+  episodHeadEl.textContent = episodeList[i].name + episodeList[i].season + episodeList[i].number;
+  episodDivEl.appendChild(episodHeadEl);
+  imageEl.src = episodeList[i].image.medium;
+  episodDivEl.appendChild(imageEl);
+  sumEpisoedEl.innerHTML = episodeList[i].summary;
+  episodDivEl.appendChild(sumEpisoedEl)
+  let origSorce = document.createElement("a");
+  origSorce.href = "https://tvmaze.com/";
+  origSorce.textContent = "This data is copy rigths, click here for more informathion";
+  episodDivEl.appendChild(origSorce)
+  
+  rootElem.appendChild(episodDivEl);
   }
-}
-let serchforEpi = document.getElementById("input");
-serchforEpi.addEventListener("keyup", function(event) {
+  }
+  let serchforEpi = document.getElementById("input");
+  serchforEpi.addEventListener("keyup", function(event) {
   input(event.target.value.toLowerCase())
-})
-function input(searchForEpis) {
-  let arrayForAllEpsi = allEpisodes.filter(episode => {
-    return episode.name.toLowerCase().includes(searchForEpis) || episode.summary.toLowerCase().includes(searchForEpis)
   })
-
-
-makePageForEpisodes(arrayForAllEpsi);
-}
-
-//Drop down search box
-
-console.log(allEpisodes);
-function getselectEpisode(episodeSilector) {
+  function input(searchForEpis) {
+  let arrayForAllEpsi = allEpisodes.filter(episode => {
+  return episode.name.toLowerCase().includes(searchForEpis) || episode.summary.toLowerCase().includes(searchForEpis)
+  })
   
   
+  makePageForEpisodes(arrayForAllEpsi);
+  }
+  
+  //Drop down search box
+  
+  function getSelectEpisode() {
   for (let i = 0; i < allEpisodes.length; i++) {
-    let selectEpisode = document.getElementById("list");
+  let selectEpisode = document.getElementById("list");
+  let option = document.createElement("option");
+  option.value = i
+  option.textContent = `S${allEpisodes[i].season} - E${allEpisodes[i].number} - ${allEpisodes[i].name}`;
+  selectEpisode.appendChild(option)
+  }
+  }
+  //Show selector
+  function getSelectShowes() {
+    for (let i = 0; i < allShowes.length; i++) {
+    let selectShows = document.getElementById("list1");
     let option = document.createElement("option");
     option.value = i
-    option.textContent = allEpisodes[i].name;
-    selectEpisode.appendChild(option)
-   
-  }
+    option.textContent = `${allShowes[i].name}`;
+    selectShows.appendChild(option)
+    }
+    }
   
-
-getselectEpisode(selecEpis)
-}
-window.onload = setup;
+  window.onload = setup;
